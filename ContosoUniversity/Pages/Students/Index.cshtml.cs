1d7efa9221fb444f4a0862d7abe3dceea1e378b2
Thomas Bishop <tombishop95@gmail.com>
2019-10-28 10:20:08 +0000

    first commit

---
+++ ContosoUniversity/Pages/Students/Index.cshtml.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
